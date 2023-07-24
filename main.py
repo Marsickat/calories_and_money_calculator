0e58127711ae81bdeb8fd4cf4fbcd383c67df516
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-24 09:48:07 +0300

    Общие шаблоны для классов

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import datetime as dt
 
 
 class Calculator:
+    def __init__(self, limit: int):
+        self.limit = limit
+        self.records = []
+
+
+class CaloriesCalculator(Calculator):
     pass
+    # TODO: метод для добавления новой записи о приёме пищи
+    # TODO: метод для подсчёта информации о количестве полученных сегодня калорий
+    # TODO: метод для определения информации о том, сколько сегодня ещё можно получить калорий
+    # TODO: метод для подсчёта информации о количестве полученных калорий за последние семь дней
+
+
+class CashCalculator(Calculator):
+    pass
+    # TODO: метод для добавления новой записи о расходах
+    # TODO: метод для подсчёта информации о количестве потраченных сегодня денег
+    # TODO: метод для определения информации о том, сколько сегодня ещё можно потратить денег (( рубли/доллары/евро ))
+    # TODO: метод для подсчёта информации о количестве потраченных денег за последние семь дней
 
 
 class Record:
-    pass
+    def __init__(self, amount: int, comment: str, date: str = dt.datetime.now().strftime("%d.%m.%Y")):
+        self.amount = amount
+        self.date = date
+        self.comment = comment
